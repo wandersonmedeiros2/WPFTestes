931765375527d0586dbb352791bd266791fd9266
wanderson.medeiros <wanderson.medeiros2@gmail.com>
2022-11-08 10:51:38 -0300

    Added Native api for change time of machine

---
+++ WpfAlarm/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAlarm.NativeApi;
+
+namespace WpfAlarm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tbNewTime_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var strtime = tbNewTime.Text;
+
+            DateTime newTime = new DateTime(2022, 11, 8, 10, 50, 32); //DateTime.Parse(strtime);
+            
+
+            Kernel32Wrapper.SetSystemTime(newTime);
+
+
+
+        }
+    }
+}
